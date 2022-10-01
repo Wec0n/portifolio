e15ed8d3f5040478cce860f75c1bcb4d4e540cd0
Willian Silva <willsoar9620@gmail.com>
2022-10-01 17:17:55 -0300

    Javascript assíncrono

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 window.onload = (() => {
-    const btn = document.querySelector('.menu-icon');
-    const menu = document.querySelector('header nav');
-    btn.addEventListener('click', () => {
-        if (menu.style.left != '0%') {
-            menu.style.left = '0%';
-            btn.querySelector('img').src = 'img/close.svg';
-        } else {
-            menu.style.left = '-100%';
-            btn.querySelector('img').src = 'img/menu.svg';
-        }
-    })
+    (() => {
+        const btn = document.querySelector('.menu-icon');
+        const menu = document.querySelector('header nav');
+        btn.addEventListener('click', () => {
+            if (menu.style.left != '0%') {
+                menu.style.left = '0%';
+                btn.querySelector('img').src = 'img/close.svg';
+            } else {
+                menu.style.left = '-100%';
+                btn.querySelector('img').src = 'img/menu.svg';
+            }
+        })
+    })();
+
+    (() => {
+        const btnSaibaMaisSiteSmec = document.querySelector('#saiba-mais-site-smec');
+
+        btnSaibaMaisSiteSmec.addEventListener('click', () => {
+            const caminhoSiteSmec = fetch('../projetos/site-smec/site-smec.html');
+            caminhoSiteSmec.then(r => r.text()).then(body => {
+                const tela = document.querySelector('main');
+                tela.innerHTML = body;
+            })
+            const removerInformacoes = document.querySelector('.informacoes');
+            removerInformacoes.remove();
+        })
+    })();
 })
